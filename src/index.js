d57058e095f4ad439ce38f25ebae6414c72e4385
SAmmyuxh <samruddhshubhadarshi@gmail.com>
2024-05-08 20:24:00 +0530

    connect to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,32 @@
+
+
+import connectDB from "./db/index.js";
+
+
+connectDB()
+
+
+
+
+
+/*
+import express from "express";
+const app =express()
+
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}`/`${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Error :",error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+
+})()
+*/
