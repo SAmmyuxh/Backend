4e4183c0af3a591a92c4b0f3eba5a33a368e0eb8
SAmmyuxh <samruddhshubhadarshi@gmail.com>
2024-05-23 18:51:39 +0530

    Multer and file upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 
 
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 
 connectDB()
 .then(()=>{
-    app.on("error",(error)=>{
-        console.log("Error :",error);
-        throw error
-    })
+    
     app.listen(process.env.PORT || 8000,()=>{
         console.log(`sERVER IS RUNNING ON ${process.env.PORT}`);
     })
